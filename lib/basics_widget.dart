fc35868d4fb63388ebcd9a26fe03e1215cbeef44
rikkaxie <619640227@qq.com>
2021-11-23 17:00:52 +0800

    basics widgets

--- lib/basics_widget.dart
+++ lib/basics_widget.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class BasicsWidgetRoute extends StatelessWidget {
+class BasicsWidgetRoute extends StatefulWidget {
   const BasicsWidgetRoute({Key? key}) : super(key: key);
+
+  @override
+  State<BasicsWidgetRoute> createState() => _BasicsWidgetRouteState();
+}
+
+class _BasicsWidgetRouteState extends State<BasicsWidgetRoute> {
+  final TextEditingController _tfController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _tfController.addListener(() {
+      print(_tfController.text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +24,25 @@ class BasicsWidgetRoute extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Basics Demo"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("i am ElevatedButton"),
-          onPressed: () {},
+      body: Column(children: <Widget>[
+        TextField(
+          controller: _tfController,
+          autofocus: true,
+          decoration: const InputDecoration(
+            labelText: "用户名",
+            hintText: "请输入用户名或密码",
+            prefixIcon: Icon(Icons.person)
+          ),
         ),
-      ),
+        const TextField(
+          decoration: InputDecoration(
+            labelText: "密码",
+            hintText: "请输入密码",
+            prefixIcon: Icon(Icons.lock),
+          ),
+          obscureText: true,
+        )
+      ]),
     );
   }
 }
